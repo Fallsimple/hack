ed38c08fbf643e874daad3b11ddb9f7636641ddf
Fallsimple <89129359+Fallsimple@users.noreply.github.com>
2021-09-05 22:19:39 +0700

    Add files via upload

---
+++ my hack.py.sh
@@ -0,0 +1,24 @@
+echo -e "\e[34mWELCOME TO TOD\e[0m";
+echo -e "\e[33mJANGAN LUPA  TOD FOLLOW AKUN IG GUA\e[0m";
+echo -e "\e[32mlogin via?  tod
+
+token
+cokiee \e[0m";
+masukkan  token tod
+read -p 'token salah tolol' userinput1;
+#
+read -p 'bener tod' userinput2;
+#
+cokiee
+masukin  cokiee ny
+read -p 'salah' userinput3;
+#
+read -p 'bener tod' userinput4;
+#
+echo -e "\e[35mpilih yg mana tod?\e[0m";
+echo -e "\e[34mid target
+id public
+massal\e[0m";
+read -p 'pilih yg benr' userinput5;
+#
+benr  tolol
